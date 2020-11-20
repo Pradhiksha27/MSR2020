2b4d2024f603f00ef821ad8738f688e44bb8916a
parul100495 <parul100495@gmail.com>
2020-11-19 19:23:18 -0500

    Restructure code for regression model

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from config import config
 from connect_database import connect_and_fetch_data
 from get_stats import get_stats
+import pandas as pd
 
 
 if __name__ == '__main__':
@@ -8,12 +9,26 @@ if __name__ == '__main__':
     params = config()
 
     # query for the database
-    query = "SELECT * from final_table3"
-
+    query = "SELECT column_name from INFORMATION_SCHEMA.columns where table_name = 'final_table3'"
     # fetch data from database
-    database_entries = connect_and_fetch_data(query, params)
+    cols = connect_and_fetch_data(query, params)
+    print("Column names: ", cols)
 
+    X_query = "SELECT snapshot_count, total_targets, original_date, num_revision, last_revision, num_releases " \
+            "FROM final_table3"
+    # fetch data from database
+    X_list = connect_and_fetch_data(X_query, params)
     # get statistics
-    get_stats(database_entries)
+    get_stats(X_list)
+    Y_query = "SELECT lifespan from final_table3"
+    # fetch data from database
+    Y_list = connect_and_fetch_data(Y_query, params)
+
+    X = pd.DataFrame.from_records(X_list, columns=['snapshot_count', 'total_targets', 'original_date',
+                                                   'num_revision', 'last_revision', 'num_releases'])
+    Y = pd.DataFrame.from_records(Y_list, columns = ['lifespan'])
+
+    print(X.shape, Y.shape)
+
 
 
